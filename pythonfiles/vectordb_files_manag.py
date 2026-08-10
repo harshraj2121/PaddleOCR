c4676db5ddfb0d8e4c86870565578f11205a9bf3
Harsh Raj <harsh.r2121@hotmail.com>
2026-08-10 15:02:58 +0530

    removed files

--- pythonfiles/vectordb_files_manag.py
+++ pythonfiles/vectordb_files_manag.py
@@ -2,9 +2,18 @@ import glob
 import os
 from llmcall import llm_call
 from ocrstructure import ocr_text_extraction, group_and_pair
+from dotenv import load_dotenv
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.vectorstores import FAISS
+load_dotenv()
+
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
 
 DB_DIRECTORY = "faiss_chunks"
 ALL_FILES = "twopdf"
+
+
 
 print("running")
 def run_all_files():
@@ -27,6 +36,18 @@ def run_all_files():
         print(chunks)
 
     return chunks
+
+output_chunks = run_all_files()
+
+
+#creating embeddings
+def creating_embeddings(chunks):
+    embedding_model = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
+    vectorstore = FAISS.from_documents(chunks, embedding_model)
+    vectorstore.save_local(DB_DIRECTORY)
+
+creating_embeddings(output_chunks)
+print("saved chunks to vector database")
 
 
 chunks = run_all_files()
