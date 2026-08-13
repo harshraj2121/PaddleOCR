abb5eaeef3d957414bb9ed6933e34cd44a78be7a
Harsh Raj <harsh.r2121@hotmail.com>
2026-08-13 14:37:57 +0530

    implementing first route in fastapi

--- app.py
+++ app.py
@@ -3,14 +3,26 @@ from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
 from langchain_core.messages import HumanMessage, SystemMessage
 from pythonfiles import search_form_database, valid_query_checker, re_ranker_function, llm_user_op
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from schemas import UserQuery
+
 
 load_dotenv()
 os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
 os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
 
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],          # Sabhi origins allow
+    allow_credentials=False,      # '*' ke saath True nahi ho sakta
+    allow_methods=["*"],          # GET, POST, PUT, DELETE...
+    allow_headers=["*"],          # Sabhi headers allow
+)
+
 
 #variables
-query = input("\nEnter query: ")
 DB_DIRECTORY = "faiss_chunks"
 system_message = (
     "you are a database manager. "
@@ -19,37 +31,45 @@ system_message = (
     "if you don't get the answer just say not found the query. "
 )
 
-
-valid_query = valid_query_checker(query)
-if valid_query == "document_query":
-    #tool variables
-    model = init_chat_model("groq:llama-3.3-70b-versatile", temperature = 0)
-    tools = [search_form_database]
-    model_with_tools = model.bind_tools(tools)
+@app.get("/")
+def test_server():
+    return "Server is running properly"
 
 
+@app.get("/userquery")
+def userquery(query: UserQuery):
+    query = query.user_query
 
-    prompt = [SystemMessage(system_message), HumanMessage(query), ]
-    tool_call_message = model_with_tools.invoke(prompt)
-    # print(tool_call_message.tool_calls)
-
-    for tool_call in tool_call_message.tool_calls:
-        selected_tools = {
-            "search_form_database": search_form_database, 
-        }[tool_call["name"].lower()]
-        tool_message = selected_tools.invoke(tool_call)
-        tool_content = tool_message.content
-
-    print("Hold on! we are gathering the information")
+    valid_query = valid_query_checker(query)
+    if valid_query == "document_query":
+        #tool variables
+        model = init_chat_model("groq:llama-3.3-70b-versatile", temperature = 0)
+        tools = [search_form_database]
+        model_with_tools = model.bind_tools(tools)
 
 
-    re_ranked_result = re_ranker_function(content= tool_content, query = query)
 
-    final_result = llm_user_op(user_query = query, reranker_op=re_ranked_result)
+        prompt = [SystemMessage(system_message), HumanMessage(query), ]
+        tool_call_message = model_with_tools.invoke(prompt)
+        # print(tool_call_message.tool_calls)
 
-    print(final_result)
+        for tool_call in tool_call_message.tool_calls:
+            selected_tools = {
+                "search_form_database": search_form_database, 
+            }[tool_call["name"].lower()]
+            tool_message = selected_tools.invoke(tool_call)
+            tool_content = tool_message.content
+
+        print("Hold on! we are gathering the information")
 
 
-else:
-    print("Enter a valid query")
+        re_ranked_result = re_ranker_function(content= tool_content, query = query)
+
+        final_result = llm_user_op(user_query = query, reranker_op=re_ranked_result)
+        print(final_result)
+        return final_result
+
+
+    else:
+        print("Enter a valid query")
 
