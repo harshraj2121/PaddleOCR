b6dc4288a3e37b65c05dd48c343102c12cf41850
Harsh Raj <harsh.r2121@hotmail.com>
2026-09-05 14:58:30 +0530

    making backend seperate

---
+++ backend/pythonfiles/__init__.py
@@ -0,0 +1,6 @@
+from backend.pythonfiles.ocrstructure import group_and_pair, ocr_text_extraction
+from backend.pythonfiles.llmcall import llm_call, valid_query_checker, llm_user_op
+from backend.pythonfiles.llm_tools import search_form_database, search_from_sql
+from backend.pythonfiles.reranker import re_ranker_function
+from backend.pythonfiles.vectordb_files_manag import run_all_files, creating_embeddings, check_if_exists_in_VDB, add_embedidngs
+
--- pythonfiles/__init__.py
+++
@@ -1,6 +0,0 @@
-from pythonfiles.ocrstructure import group_and_pair, ocr_text_extraction
-from pythonfiles.llmcall import llm_call, valid_query_checker, llm_user_op
-from pythonfiles.llm_tools import search_form_database, search_from_sql
-from pythonfiles.reranker import re_ranker_function
-from pythonfiles.vectordb_files_manag import run_all_files, creating_embeddings, check_if_exists_in_VDB, add_embedidngs
-
