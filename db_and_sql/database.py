e8ef6c71d740e73837f0a9e8e1e42ed55bb8d88f
Harsh Raj <harsh.r2121@hotmail.com>
2026-09-05 22:12:06 +0530

    Connecting to Supabase

--- db_and_sql/database.py
+++ db_and_sql/database.py
@@ -1,7 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-DATABASE_URL = "mysql+pymysql://root:harsh1234@localhost/paddleocr"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# DATABASE_URL = "mysql+pymysql://root:harsh1234@localhost/paddleocr"
+
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
 
