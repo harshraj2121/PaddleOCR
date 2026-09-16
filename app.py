8fbc994fb3929bef871a3c56ffc7d277c68d71da
Harsh Raj <harsh.r2121@hotmail.com>
2026-09-16 22:29:58 +0530

    making the final changes

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://paddle-ocr.vercel.app",
     ],         # Sabhi origins allow
     allow_credentials=False,      # '*' ke saath True nahi ho sakta
     allow_methods=["*"],          # GET, POST, PUT, DELETE...
